9f77cc2d07057ff28652467a478088040aca5242
Andres Perez Campillo <tomailsandstuff@gmail.com>
2022-12-27 17:11:12 -0500

    Update CIBuilder.cs

--- Assets/Editor/CIBuilder.cs
+++ Assets/Editor/CIBuilder.cs
@@ -1,5 +1,6 @@
 using UnityEditor;
 using UnityEditor.Build.Reporting;
+using UnityEngine;
 
 public class CIBuilder
 {
@@ -13,5 +14,8 @@ public class CIBuilder
         buildOptions.options = BuildOptions.None;
 
         BuildReport report = BuildPipeline.BuildPlayer(buildOptions);
+        BuildSummary summary = report.summary;
+
+        Debug.Log($"Build completed with result [{summary.result}]: {(summary.result == BuildResult.Succeeded ? (summary.totalSize + " bytes") : string.Empty)}");
     }
 }
